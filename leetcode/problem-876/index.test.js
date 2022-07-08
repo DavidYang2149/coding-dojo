72e2ae4670aa945378f10b6b7051569d16e466ea
davidyang2149 <davidyang2149@gmail.com>
2022-07-08 17:14:45 +0900

    릿코드 876번 초기화

---
+++ leetcode/problem-876/index.test.js
@@ -0,0 +1,6 @@
+const reverseList = require('./index');
+
+test('876', () => {
+  expect(reverseList([1,2,3,4,5])).toStrictEqual([3,4,5]);
+  expect(reverseList([1,2,3,4,5,6])).toStrictEqual([4,5,6]);
+});
