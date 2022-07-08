69ee8de5660f3490756acf39102ff32bbfc75c5f
davidyang2149 <davidyang2149@gmail.com>
2022-07-08 16:15:59 +0900

    릿코드 206번 문제 초기화

---
+++ leetcode/problem-206/index.test.js
@@ -0,0 +1,7 @@
+const reverseList = require('./index');
+
+test('206', () => {
+  expect(reverseList([1,2,3,4,5])).toStrictEqual([5,4,3,2,1]);
+  expect(reverseList([1,2])).toStrictEqual([2, 1]);
+  expect(reverseList([])).toStrictEqual([]);
+});
