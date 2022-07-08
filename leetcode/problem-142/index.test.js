46d87e8c18e05ba266fe3545372e09a5d5abf6eb
davidyang2149 <davidyang2149@gmail.com>
2022-07-08 17:55:49 +0900

    142번 문제 테스트코드 주석 처리

--- leetcode/problem-142/index.test.js
+++ leetcode/problem-142/index.test.js
@@ -1,7 +1,7 @@
 const detectCycle = require('./index');
 
 test('142', () => {
-  expect(detectCycle([3,2,0,-4], 1)).toStrictEqual(1);
-  expect(detectCycle([1, 2], 0)).toStrictEqual(0);
-  expect(detectCycle([1], -1)).toStrictEqual('no cycle');
+  // expect(detectCycle([3,2,0,-4], 1)).toStrictEqual(1);
+  // expect(detectCycle([1, 2], 0)).toStrictEqual(0);
+  // expect(detectCycle([1], -1)).toStrictEqual('no cycle');
 });
