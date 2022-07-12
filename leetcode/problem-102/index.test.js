8118bc6dc3e1ac95d4894fbd77f1cbbd5e64f2ef
davidyang2149 <davidyang2149@gmail.com>
2022-07-12 20:07:28 +0900

    릿코드 102번 초기화

---
+++ leetcode/problem-102/index.test.js
@@ -0,0 +1,7 @@
+const levelOrder = require('./index');
+
+test('102', () => {
+  // expect(levelOrder([3,9,20,null,null,15,7])).toStrictEqual([[3],[9,20],[15,7]]);
+  // expect(levelOrder([1])).toStrictEqual([1]);
+  // expect(levelOrder([])).toStrictEqual([]);
+});
