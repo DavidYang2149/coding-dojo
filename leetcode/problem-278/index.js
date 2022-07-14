a845d0810c8d7a54feb6df021a428c6842029b9b
davidyang2149 <davidyang2149@gmail.com>
2022-07-14 21:25:28 +0900

    릿코드 278 문제 풀이(답지)

--- leetcode/problem-278/index.js
+++ leetcode/problem-278/index.js
@@ -51,7 +51,21 @@ var solution = function(isBadVersion) {
    * @return {integer} The first bad version
    */
   return function(n) {
+    let min = n;
+    let left = 0, right = n - 1;
     
+    while (left <= right) {
+      const i = left + Math.floor((right - left) / 2);
+    
+      if (isBadVersion(i + 1)) {
+          min = Math.min(min, i);
+          right = i - 1;
+      } else {
+        left = i + 1;
+      }
+    }
+
+    return min + 1;
   };
 };
 
