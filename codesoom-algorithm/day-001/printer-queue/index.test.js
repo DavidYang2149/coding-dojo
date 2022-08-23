52e9692115ac56e02a29850eefdcc068af67bc33
Yang Hong Seok <davidyang2149@gmail.com>
2022-08-23 11:07:29 +0900

    :white_check_mark: 프린터 큐의 테스트 코드를 올바르게 작성합니다

--- codesoom-algorithm/day-001/printer-queue/index.test.js
+++ codesoom-algorithm/day-001/printer-queue/index.test.js
@@ -1,7 +1,7 @@
 const solution = require('./index');
 
 test('printer-queue', () => {
-  expect(solution()).toBe(1);
-  expect(solution()).toBe(2);
-  expect(solution()).toBe(5);
+  expect(solution('1 0', '5')).toBe(1);
+  expect(solution('4 2', '1 2 3 4')).toBe(2);
+  expect(solution('6 0', '1 1 9 1 1 1')).toBe(5);
 });
